0c70584289e1e9186e5fbad974603a00b847e0ed
hsandorf <165305090+hsandorf@users.noreply.github.com>
2026-06-09 14:07:33 -0400

    Refactor button handling for book selection

--- just_books.py
+++ just_books.py
@@ -10,11 +10,11 @@ from google.oauth2.service_account import Credentials
 
 st.markdown("""
    <style>
-/*   [data-testid="stImage"] img {
+   [data-testid="stImage"] img {
         height: 200px;
         width: 150px;
         object-fit: cover;
-      } */
+      } 
     [data-testid="stHorizontalBlock"] > div .stButton button {
         display: block;
         margin: 0 auto;
@@ -189,27 +189,33 @@ with col1:
     st.caption(book1["title"])
     st.caption(f'Read: {book1["date_read"]}')
     st.caption(book1["author"])
-    if st.button(book1["title"], key="btn1"):
-        record_result(book1, book2, winner=book1["title"])
-        st.session_state.last_winner = book1["title"]
-        st.session_state.current_pair = get_next_pair()
-        st.rerun()
+    
 
 
     st.image(load_image(book2["image_path"]))
     st.caption(book2["title"])
     st.caption(f'Read: {book2["date_read"]}')
     st.caption(book2["author"])
+   
+
+st.write("") # vertical spacing
+st.write("")
+
+with col3:
+
+   if st.button(book1["title"], key="btn1"):
+        record_result(book1, book2, winner=book1["title"])
+        st.session_state.last_winner = book1["title"]
+        st.session_state.current_pair = get_next_pair()
+        st.rerun()
+
     if st.button(book2["title"], key="btn2"):
         record_result(book1, book2, winner=book2["title"])
         st.session_state.last_winner = book2["title"]
         st.session_state.current_pair = get_next_pair()
         st.rerun()
 
-st.write("") # vertical spacing
-st.write("")
-
-with col3:
+   
    if st.button("I haven't read either", key="btn3"):
        st.session_state.unread_books.update([book1["title"], book2["title"]])
        record_result(book1, book2, winner=None, book1_unread=True, book2_unread=True)
@@ -217,14 +223,14 @@ with col3:
        st.session_state.current_pair = get_next_pair()
        st.rerun()
    
-   if st.button(f" < I haven't read:\n{book1['title']}", key="btn4"):
+   if st.button(f"I haven't read:\n{book1['title']}", key="btn4"):
        st.session_state.unread_books.add(book1["title"])
        record_result(book1, book2, winner=None, book1_unread=True)
        st.session_state.last_winner = "Neither"
        st.session_state.current_pair = get_next_pair()
        st.rerun()
    
-   if st.button(f" > I haven't read:\n{book2['title']}", key="btn5"):
+   if st.button(f"I haven't read:\n{book2['title']}", key="btn5"):
        st.session_state.unread_books.add(book2["title"])
        record_result(book1, book2, winner=None, book2_unread=True)
        st.session_state.last_winner = "Neither"
